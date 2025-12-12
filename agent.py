c1ceb76519962a6baf24349f1f15c20b2bc0dd21
NoahBradberry <nbradberry425@phm.k12.in.us>
2025-12-12 09:37:59 -0500

    almost there

--- agent.py
+++ agent.py
@@ -23,7 +23,7 @@ class Agent():
 
         obs, info = self.env.reset()
 
-        # TODO - Process  Observation
+        obs = self.process_observation(obs)
 
         self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
@@ -42,6 +42,100 @@ class Agent():
         self.learning_rate = learning_rate
 
 
-    # 1:37:07 Problems to fix
+    def process_observation(self, obs):
+        obs = torch.tensor(obs, dtype = torch.float32).permute(2, 0, 1)
+        return obs
+    
+    def train(self, episodes, max_episode_steps, summary_writer_suffix, batch_size, epsilon, epsilon_decay, min_epsilon):
 
-        
+        summary_writer_name = f'runs/{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_{summary_writer_suffix}'
+        writer = SummaryWriter(summary_writer_name)
+
+        if not os.path.exists('models'):
+            os.makedirs('models')
+
+        total_steps = 0
+
+        for episode in range(episodes):
+
+            done = False
+            episode_reward = 0
+            obs, info = self.env.reset()
+            obs = self.process_observation(obs)
+
+            episode_steps = 0
+
+            episode_start_time = time.time()
+
+            while not done and episode_steps < max_episode_steps:
+
+                if random.random() < epsilon:
+                    action = self.env.action_space_sample()
+                else:
+                    q_values = self.model.forward(obs.unsqueeze(0).to(self.device))[0]
+                    action = torch.argmax(q_values, dim = -1).item()
+                
+                reward = 0
+
+                for i in range(self.step_repeat):
+                    reward_temp = 0
+
+                    next_obs, reward_temp, done, truncated, info = self.env.step(action = action)
+
+                    reward += reward_temp
+
+                    if(done):
+                        break
+
+                next_obs = self.process_observation(next_obs)
+
+                self.memory.store_transition(obs, action, reward, next_obs, done)
+
+                obs = next_obs
+
+                episode_reward += reward
+                episode_steps += 1
+                total_steps += 1
+
+                if self.memory.can_sample(batch_size):
+
+                    observations, actions, rewards, next_observations, dones = self.memory.sample_buffer(batch_size)
+
+                    dones = dones.unsqueeze(1).float()
+
+                    # Current Q-Values from both models
+                    q_values = self.model(observations)
+                    actions = actions.unsqueeze(1).long()
+                    qsa_batch = q_values.gather(1, actions)
+
+                    next_actions = torch.argmax(self.model(next_observations), dim = 1, keepdim = True)
+
+                    next_q_values = self.target_model(next_observations).gather(1, next_actions)
+
+                    target_b = rewards.unsqueeze(1) + (1 - dones) * self.gamma * next_q_values
+
+                    loss = F.mse_loss(qsa_batch, target_b.detach())
+
+                    writer.add_scalar("Loss/model", loss.item(), total_steps)
+
+                    self.model.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+
+                    if episode_steps % 4 == 0:
+                        soft_update(self.target_model, self.model)
+
+
+            self.model.save_the_model()
+
+            writer.add_scalar("Score", episode_reward, episode)
+            writer.add_scalar("Epsilon", epsilon, episode)
+
+            if epsilon > min_epsilon:
+                epsilon *= epsilon_decay
+
+            episode_time = time.time() - episode_start_time
+
+            print(f"Completed episode {episode} with score {episode_reward}")
+            print(f"Episode Time: {episode_time : 1f} seconds")
+            print(f"Episode Steps: {episode_steps}")
